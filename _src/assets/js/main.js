033a986e21bb4f64d18d93c521db6e5d85c21ece
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-08 13:16:47 +0200

    create an array to push favorite elements and change the style

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,15 +5,39 @@ const finder = document.querySelector('.js__finder__search');
 const seriesList = document.querySelector('.js__series__list');
 const input = document.querySelector('.js__finder__input');
 
+//Guardar en favoritos
+
+let favs = [];
+
+function handleFavs(event) {
+  const item = event.currentTarget;
+  const name = item.querySelector.name
+  item.classList.toggle('series-favorite__element');
+  if (item.classList.contains('series-favorite__element')) {
+    // lo guardo en el array solo si no existe
+
+    if (favs.includes(name) === false) {
+      favs.push(name);
+    }
+
+  } else {
+    // lo quito del array
+    const index = favs.indexOf(name);
+    if (index > -1) {
+      favs.splice(index, 1);
+    }
+  }
+  console.log(favs);
+}
 
 
+//Buscar series en la Api
 function searchSeries() {
   const seriesSearch = input.value;
   seriesList.innerHTML= '';
   fetch(`http://api.tvmaze.com/search/shows?q=${seriesSearch}`)
     .then(response => response.json())
     .then(series => {
-      console.log(series);
       for (const serie of series) {
         const elementResult = document.createElement('li');
         elementResult.classList.add('series__element');
@@ -36,6 +60,12 @@ function searchSeries() {
         elementResult.appendChild(imageResult);
         elementResult.appendChild(titleResult);
         seriesList.appendChild(elementResult);
+
+        const saveFavorites = document.querySelectorAll('.series__element');
+
+        for (let i=0;i<saveFavorites.length;i++) {
+          saveFavorites[i].addEventListener('click', handleFavs);
+        }
       }
     });
 }
@@ -44,3 +74,8 @@ searchSeries();
 
 finder.addEventListener('click',searchSeries);
 
+//Guardar en favoritos
+
+//const favoriteListe = document.querySelector('.series-favorite__list');
+
+//seriesList.addEventListener('click', moveFavorite);
