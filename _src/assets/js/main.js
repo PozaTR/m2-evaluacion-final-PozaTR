64caa06c8f368b1f96d203afcd935e2f140f7ac7
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-08 09:34:13 +0200

    prepare the listener in the input

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
+const finder = document.querySelector('.js__finder__container');
 
+function searchSeries(event) {
+  event.preventDefault();
+  console.log('funciono');
+}
+
+
+
+
+finder.addEventListener('submit', searchSeries);
