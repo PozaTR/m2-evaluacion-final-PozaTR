b32d129e4e800edd1224778f13e28af860a581f6
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-08 11:30:36 +0200

    create elements of the list in js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,16 +2,33 @@
 
 //const finder = document.querySelector('.js__finder__container');
 const finder = document.querySelector('.js__finder__search');
-const seriesList = document.querySelector('.series__list');
+const seriesList = document.querySelector('.js__series__list');
 const input = document.querySelector('.js__finder__input');
 
 function searchSeries() {
   const seriesSearch = input.value;
   seriesList.innerHTML= '';
+
   fetch(`http://api.tvmaze.com/search/shows?q=${seriesSearch}`)
     .then(response => response.json())
     .then(series => {
       console.log(series);
+      /*for (const serie of series) {
+        const elementResult = document.createElement('li');
+        elementResult.classList.add('series__element');
+
+        const imageResult = document.createElement('div');
+        imageResult.classList.add('series__element__img');
+        imageResult.style.backgroundImage = `url('${series.show.image.medium}')`;
+
+        const titleResult = document.createElement('p');
+        titleResult.classList.add('series__element__title');
+        titleResult.innerHTML = series.show.name;
+
+        elementResult.appendChild(imageResult);
+        elementResult.appendChild(titleResult);
+        seriesList.appendChild(elementResult);
+      }*/
     });
 }
 
