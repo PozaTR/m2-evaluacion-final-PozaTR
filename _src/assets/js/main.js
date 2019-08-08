4b298b16d655961b27bfd7acd027a921f636b6d7
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-08 10:03:28 +0200

    create the conexion with the api

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,13 +1,21 @@
 'use strict';
 
-const finder = document.querySelector('.js__finder__container');
+//const finder = document.querySelector('.js__finder__container');
+const finder = document.querySelector('.js__finder__search');
+const seriesList = document.querySelector('.series__list');
+const input = document.querySelector('.js__finder__input');
 
-function searchSeries(event) {
-  event.preventDefault();
-  console.log('funciono');
+function searchSeries() {
+  const seriesSearch = input.value;
+  seriesList.innerHTML= '';
+  fetch(`http://api.tvmaze.com/search/shows?q=${seriesSearch}`)
+    .then(response => response.json())
+    .then(series => {
+      console.log(series);
+    });
 }
 
+searchSeries();
 
+finder.addEventListener('click',searchSeries);
 
-
-finder.addEventListener('submit', searchSeries);
