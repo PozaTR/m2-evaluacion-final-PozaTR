ca517ea91a4eda108e77967e858df382f2406204
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-08 12:06:27 +0200

    fetch all the elements we need

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,30 +5,32 @@ const finder = document.querySelector('.js__finder__search');
 const seriesList = document.querySelector('.js__series__list');
 const input = document.querySelector('.js__finder__input');
 
+
+
 function searchSeries() {
   const seriesSearch = input.value;
   seriesList.innerHTML= '';
-
+  //const endpoint = `http://api.tvmaze.com/search/shows?q=${seriesSearch}`;
   fetch(`http://api.tvmaze.com/search/shows?q=${seriesSearch}`)
     .then(response => response.json())
     .then(series => {
       console.log(series);
-      /*for (const serie of series) {
+      for (const serie of series) {
         const elementResult = document.createElement('li');
         elementResult.classList.add('series__element');
 
         const imageResult = document.createElement('div');
         imageResult.classList.add('series__element__img');
-        imageResult.style.backgroundImage = `url('${series.show.image.medium}')`;
+        imageResult.style.backgroundImage = `url('${serie.show.image.medium}')`;
 
         const titleResult = document.createElement('p');
         titleResult.classList.add('series__element__title');
-        titleResult.innerHTML = series.show.name;
+        titleResult.innerHTML = serie.show.name;
 
         elementResult.appendChild(imageResult);
         elementResult.appendChild(titleResult);
         seriesList.appendChild(elementResult);
-      }*/
+      }
     });
 }
 
